4fb2518a8e8a50b1dac424d13d946e5c78a33d58
Chad Thielen <cthiel42>
2022-10-28 14:52:58 -0500

    added lambda, example config, and readme contents

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# dynamodb-scheduled-autoscaler
+# DynamoDB Scheduled Autoscaler
+
+### Overview & Use Cases
+This is not a full blown replacement for DynamoDB autoscaling, but something that can be paired with DynamoDB autoscaling to greatly enhance it's functionality. In its current state, this Lambda can be used to adjust the Minimum and Maximum Read/Write unit capacities for DynamoDB on a set schedule. This is somewhat similar to Autoscaling Scheduled Actions, but allows you to specify your scaling rules in a live configuration file.
+
+The most common use case for this would be when you have a very predictable spike in read/writes, and you want to scale up prior to that spike so you don't get any throttled requests. In this case, you might create an interval where the minimum capacity units greatly increase to handle the burst, and then sometime after the burst or even during the burst, you can end that interval to let the capacities go back to their default. Since this is best if it's paired with DynamoDB's autoscaling, reducing capacities before the burst is over won't cause provisioned capacities to be reduced, as they'll continue to track the target utilization specified in the autoscaling settings.
+
+### Setup & Configuration
+This can be run as a Lambda function with an EventBridge rule as the trigger. My recommendation would be to not run the Lambda any more frequently than every 5 minutes. DynamoDB tables can take several minutes to update with new autoscale settings, so it's not useful to be running on a more frequent interval than roughly 5 minutes. The Lambda will also need `application-autoscaling:DescribeScalableTargets` and `application-autoscaling:RegisterScalableTarget` permissions to update table capacities.
+
+The configuration file takes in a JSON array of settings. Each setting is for a specific table or index, and can contain several different scaling intervals. All times in the configuration file should be specified in UTC time. See the included config file for examples and a template.
+
+Configurations that need to be included in a setting:
+- type: table|index (str)
+- resource: scaling policy resource (str)
+- defaultReadMin: minimum read capacity to default to if no intervals are active (int)
+- defaultReadMax: maximum read capacity to default to if no intervals are active (int)
+- defaultWriteMin: minimum write capacity to default to if no intervals are active (int)
+- defaultWriteMax: maximum write capacity to default to if no intervals are active (int)
+- intervals
+  - timeStart: time the interval settings should take effect in UTC and format of HH:MM:SS (str)
+  - timeEnd: time the interval settings should discontinue taking effect in UTC and format of HH:MM:SS (str)
+  - readMin: (Optional) minimum read capacity to set while interval is in effect (int)
+  - readMax: (Optional) maximum read capacity to set while interval is in effect (int)
+  - writeMin: (Optional) minimum write capacity to set while interval is in effect (int)
+  - writeMax: (Optional) maximum write capacity to set while interval is in effect (int)
---
+++ lambda_function.py
@@ -0,0 +1,88 @@
+import json
+import boto3
+import datetime
+import os
+
+def lambda_handler(event, context):
+    client = boto3.client('application-autoscaling',region_name=os.environ['AWS_REGION'])
+    file = open('config.json','r')
+    config = file.read()
+    file.close()
+    config = json.loads(config)
+    
+    for operation in config:
+        print(operation.get("resource"))
+        # determine what scaling settings should be at default
+        readMin = operation.get("defaultReadMin")
+        readMax = operation.get("defaultReadMax")
+        writeMin = operation.get("defaultWriteMin")
+        writeMax = operation.get("defaultWriteMax")
+        currentTime = datetime.datetime.utcnow()
+        for interval in operation.get("intervals"):
+            # Check whether we're in any intervals that will overwrite the 
+            intervalStart = datetime.datetime.strptime(interval.get("timeStart"),"%H:%M:%S")
+            intervalEnd = datetime.datetime.strptime(interval.get("timeEnd"),"%H:%M:%S")
+            intervalStart = intervalStart.replace(year=currentTime.year,month=currentTime.month,day=currentTime.day)
+            intervalEnd = intervalEnd.replace(year=currentTime.year,month=currentTime.month,day=currentTime.day)
+            
+            # Currently won't handle overlapping intervals. Could write it in the future to take the greatest min/max values it sees
+            if intervalStart <= currentTime and currentTime < intervalEnd:
+                readMin = interval.get("readMin",readMin)
+                readMax = interval.get("readMax",readMax)
+                writeMin = interval.get("writeMin",writeMin)
+                writeMax = interval.get("writeMax",writeMax)
+            
+            print("Determined the following values should be set: ")
+            print("readMin: "+str(readMin))
+            print("readMax: "+str(readMax))
+            print("writeMin: "+ str(writeMin))
+            print("writeMax: "+str(writeMax))
+            
+            dimension = 'dynamodb:' + operation.get("type") + ':ReadCapacityUnits'
+            response = client.describe_scalable_targets(
+                ServiceNamespace='dynamodb',
+                ScalableDimension=dimension,
+                ResourceIds=[
+                    operation.get("resource")
+                ],
+                MaxResults=10,
+            )
+            
+            if len(response.get("ScalableTargets",[])) == 1 and readMin != response.get("ScalableTargets")[0].get("MinCapacity") or readMax != response.get("ScalableTargets")[0].get("MaxCapacity"):
+                print("Updating Read Capacity Units:")
+                print("ReadMin: "+str(response.get("ScalableTargets")[0].get("MinCapacity")) + " -> " + str(readMin))
+                print("ReadMax: "+str(response.get("ScalableTargets")[0].get("MaxCapacity")) + " -> " + str(readMax))
+                response = client.register_scalable_target(
+                    ServiceNamespace='dynamodb',
+                    ResourceId=operation.get("resource"),
+                    ScalableDimension=dimension,
+                    MinCapacity=readMin,
+                    MaxCapacity=readMax
+                )
+
+            dimension = 'dynamodb:' + operation.get("type") + ':WriteCapacityUnits'
+            response = client.describe_scalable_targets(
+                ServiceNamespace='dynamodb',
+                ScalableDimension=dimension,
+                ResourceIds=[
+                    operation.get("resource")
+                ],
+                MaxResults=10,
+            )
+            
+            if len(response.get("ScalableTargets",[])) == 1 and writeMin != response.get("ScalableTargets")[0].get("MinCapacity") or writeMax != response.get("ScalableTargets")[0].get("MaxCapacity"):
+                print("Updating Write Capacity Units:")
+                print("WriteMin: "+str(response.get("ScalableTargets")[0].get("MinCapacity")) + " -> " + str(writeMin))
+                print("WriteMax: "+str(response.get("ScalableTargets")[0].get("MaxCapacity")) + " -> " + str(writeMax))
+                response = client.register_scalable_target(
+                    ServiceNamespace='dynamodb',
+                    ResourceId=operation.get("resource"),
+                    ScalableDimension=dimension,
+                    MinCapacity=writeMin,
+                    MaxCapacity=writeMax
+                )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Lambda exited successfully')
+    }
