b137ca9219c9dc860cc4094e82867ff67b8f01f9
Chad Thielen <48074114+cthiel42@users.noreply.github.com>
2023-01-05 09:52:01 -0600

    Indentation issues

--- lambda_function.py
+++ lambda_function.py
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
         writeMax = operation.get("defaultWriteMax")
         currentTime = datetime.datetime.utcnow()
         for interval in operation.get("intervals"):
-            # Check whether we're in any intervals that will overwrite the 
+            # Check whether we're in any intervals that will overwrite the existing capacity
             intervalStart = datetime.datetime.strptime(interval.get("timeStart"),"%H:%M:%S")
             intervalEnd = datetime.datetime.strptime(interval.get("timeEnd"),"%H:%M:%S")
             intervalStart = intervalStart.replace(year=currentTime.year,month=currentTime.month,day=currentTime.day)
@@ -32,55 +32,55 @@ def lambda_handler(event, context):
                 writeMin = interval.get("writeMin",writeMin)
                 writeMax = interval.get("writeMax",writeMax)
             
-            print("Determined the following values should be set: ")
-            print("readMin: "+str(readMin))
-            print("readMax: "+str(readMax))
-            print("writeMin: "+ str(writeMin))
-            print("writeMax: "+str(writeMax))
-            
-            dimension = 'dynamodb:' + operation.get("type") + ':ReadCapacityUnits'
-            response = client.describe_scalable_targets(
-                ServiceNamespace='dynamodb',
-                ScalableDimension=dimension,
-                ResourceIds=[
-                    operation.get("resource")
-                ],
-                MaxResults=10,
-            )
-            
-            if len(response.get("ScalableTargets",[])) == 1 and readMin != response.get("ScalableTargets")[0].get("MinCapacity") or readMax != response.get("ScalableTargets")[0].get("MaxCapacity"):
-                print("Updating Read Capacity Units:")
-                print("ReadMin: "+str(response.get("ScalableTargets")[0].get("MinCapacity")) + " -> " + str(readMin))
-                print("ReadMax: "+str(response.get("ScalableTargets")[0].get("MaxCapacity")) + " -> " + str(readMax))
-                response = client.register_scalable_target(
-                    ServiceNamespace='dynamodb',
-                    ResourceId=operation.get("resource"),
-                    ScalableDimension=dimension,
-                    MinCapacity=readMin,
-                    MaxCapacity=readMax
-                )
+        print("Determined the following values should be set: ")
+        print("readMin: "+str(readMin))
+        print("readMax: "+str(readMax))
+        print("writeMin: "+ str(writeMin))
+        print("writeMax: "+str(writeMax))
 
-            dimension = 'dynamodb:' + operation.get("type") + ':WriteCapacityUnits'
-            response = client.describe_scalable_targets(
+        dimension = 'dynamodb:' + operation.get("type") + ':ReadCapacityUnits'
+        response = client.describe_scalable_targets(
+            ServiceNamespace='dynamodb',
+            ScalableDimension=dimension,
+            ResourceIds=[
+                operation.get("resource")
+            ],
+            MaxResults=10,
+        )
+
+        if len(response.get("ScalableTargets",[])) == 1 and readMin != response.get("ScalableTargets")[0].get("MinCapacity") or readMax != response.get("ScalableTargets")[0].get("MaxCapacity"):
+            print("Updating Read Capacity Units:")
+            print("ReadMin: "+str(response.get("ScalableTargets")[0].get("MinCapacity")) + " -> " + str(readMin))
+            print("ReadMax: "+str(response.get("ScalableTargets")[0].get("MaxCapacity")) + " -> " + str(readMax))
+            response = client.register_scalable_target(
                 ServiceNamespace='dynamodb',
+                ResourceId=operation.get("resource"),
                 ScalableDimension=dimension,
-                ResourceIds=[
-                    operation.get("resource")
-                ],
-                MaxResults=10,
+                MinCapacity=readMin,
+                MaxCapacity=readMax
             )
-            
-            if len(response.get("ScalableTargets",[])) == 1 and writeMin != response.get("ScalableTargets")[0].get("MinCapacity") or writeMax != response.get("ScalableTargets")[0].get("MaxCapacity"):
-                print("Updating Write Capacity Units:")
-                print("WriteMin: "+str(response.get("ScalableTargets")[0].get("MinCapacity")) + " -> " + str(writeMin))
-                print("WriteMax: "+str(response.get("ScalableTargets")[0].get("MaxCapacity")) + " -> " + str(writeMax))
-                response = client.register_scalable_target(
-                    ServiceNamespace='dynamodb',
-                    ResourceId=operation.get("resource"),
-                    ScalableDimension=dimension,
-                    MinCapacity=writeMin,
-                    MaxCapacity=writeMax
-                )
+
+        dimension = 'dynamodb:' + operation.get("type") + ':WriteCapacityUnits'
+        response = client.describe_scalable_targets(
+            ServiceNamespace='dynamodb',
+            ScalableDimension=dimension,
+            ResourceIds=[
+                operation.get("resource")
+            ],
+            MaxResults=10,
+        )
+
+        if len(response.get("ScalableTargets",[])) == 1 and writeMin != response.get("ScalableTargets")[0].get("MinCapacity") or writeMax != response.get("ScalableTargets")[0].get("MaxCapacity"):
+            print("Updating Write Capacity Units:")
+            print("WriteMin: "+str(response.get("ScalableTargets")[0].get("MinCapacity")) + " -> " + str(writeMin))
+            print("WriteMax: "+str(response.get("ScalableTargets")[0].get("MaxCapacity")) + " -> " + str(writeMax))
+            response = client.register_scalable_target(
+                ServiceNamespace='dynamodb',
+                ResourceId=operation.get("resource"),
+                ScalableDimension=dimension,
+                MinCapacity=writeMin,
+                MaxCapacity=writeMax
+            )
     
     return {
         'statusCode': 200,
